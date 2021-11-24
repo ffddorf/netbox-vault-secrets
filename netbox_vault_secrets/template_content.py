ce703401c959de373abbf5d69dd886c1e624dc36
Marcus Weiner <marcus.weiner@gmail.com>
2021-11-24 02:32:52 +0100

    Make some context data available

--- netbox_vault_secrets/template_content.py
+++ netbox_vault_secrets/template_content.py
@@ -5,7 +5,12 @@ class VaultDeviceSecrets(PluginTemplateExtension):
     model = 'dcim.device'
 
     def right_page(self):
-        return self.render('netbox_vault_secrets/secrets.html', extra_context={})
+        script_data = {
+            "object_id": self.context["object"].id,
+        }
+        return self.render('netbox_vault_secrets/secrets.html', extra_context={
+            "script_data": script_data
+        })
 
 
 template_extensions = [VaultDeviceSecrets]
