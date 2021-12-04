98882edbb9980b441521ac58cd712c856502e03d
Marcus Weiner <marcus.weiner@gmail.com>
2021-12-04 01:58:47 +0100

    Use shorter secret paths

--- netbox_vault_secrets/template_content.py
+++ netbox_vault_secrets/template_content.py
@@ -4,7 +4,7 @@ from extras.plugins import PluginTemplateExtension
 class VaultSecretInserter(PluginTemplateExtension):
     def right_page(self):
         script_data = {
-            "objectPath": f"netbox/{self.model}/{self.context['object'].id}",
+            "objectPath": f"netbox/{self.vault_path_slug}/{self.context['object'].id}",
         }
         return self.render('netbox_vault_secrets/secrets.html', extra_context={
             "script_data": script_data
@@ -12,6 +12,7 @@ class VaultSecretInserter(PluginTemplateExtension):
 
 
 class VaultDeviceSecrets(VaultSecretInserter):
+    vault_path_slug = "device"
     model = 'dcim.device'
 
 
