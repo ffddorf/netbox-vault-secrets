a2447b112dcaa24982846c884f2537501d9374e4
Marcus Weiner <marcus.weiner@gmail.com>
2021-12-05 22:17:44 +0100

    Lower initial version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='netbox_vault_secrets',
-    version='0.1',
+    version='0.0.1',
     description='NetBox Plugin to manage vault secrets from Netbox',
     url='https://github.com/ffddorf/netbox-vault-secrets',
     author='Marcus Weiner',
